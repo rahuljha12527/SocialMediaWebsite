80ed29dc7037d619ee9c54a81b684453d262ee2c
rahuljha661 <rahuljhamansapur@gmail.com>
2020-04-22 19:32:55 +0530

    sign up page completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User=require('../models/user');
+
 module.exports.profile=function(req,res){
    return res.render('users_profile',{
        title:'users'
@@ -17,9 +19,36 @@ module.exports.signIn=function(req,res){
 }
 
 module.exports.create=function(req,res){
-    // TODO later 
+  
+    if(req.body.password!=req.body.confirm_password){
+           return res.redirect('back');       
+    }
+
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){
+            console.log('error in finding user in sigining up');
+            return;
+        }
+
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log('error in creating user while sigining up');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
+    })
+
+
 }
 // sign inand create a session for a user
 module.exports.createSession=function(req,res){
-    // Todo later
+    
+    
+
 }
--- routes/users.js
+++ routes/users.js
@@ -12,5 +12,7 @@ router.get('/sign-in',usersController.signIn);
 
 router.get('/sign-up',usersController.signUp);
 
+router.post('/create',usersController.create);
+
 module.exports=router;
 
