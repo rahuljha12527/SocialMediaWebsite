e7beb67f09ec032db969e10288a61fa23f4b5dae
rahuljha661 <rahuljhamansapur@gmail.com>
2020-04-23 23:33:26 +0530

    added scss file

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ const passportLocal=require('./config/passport-local-strategy');
 
 const MongoStore=require('connect-mongo')(session);
 
+const sassMiddleware=require('node-sass-middleware');
 
+app.use(sassMiddleware({
+   src: './assets/scss',
+   dest: './assets/css',
+   debug:true,
+   outputStyle: 'extended',
+   prefix: '/css'
+}));
 
 app.use(express.urlencoded());
 
