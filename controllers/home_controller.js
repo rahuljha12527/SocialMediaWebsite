1162378d308b75e37f760bcec5420044414b5f98
rahuljha661 <rahuljhamansapur@gmail.com>
2020-05-01 14:55:19 +0530

    display  all comments pre populationg all data

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -12,8 +12,16 @@ module.exports.home=function(req,res){
     // })
 
     // Populate the user of each post
-    Post.find({}).populate('user').exec(function(err,posts){
-        return res.render('home',{
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+           path:'user'
+        }
+    })
+    .exec(function(err,posts){
+      return res.render('home',{
             title:"Social | Home",
             posts:posts
          });
--- models/post.js
+++ models/post.js
@@ -16,7 +16,7 @@ const postSchema=new mongoose.Schema({
     [
     {
         type:mongoose.Schema.Types.ObjectId,
-        ref:'comment'
+        ref:'Comment'
     }
 ]
 },{
