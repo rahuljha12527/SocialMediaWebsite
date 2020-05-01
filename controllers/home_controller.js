876d20a5baaa03892000ebce085374bf20bddbdb
rahuljha661 <rahuljhamansapur@gmail.com>
2020-05-01 12:42:13 +0530

    creating the post or put the auther of post and put all the post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,24 @@
+const Post=require('../models/post');
+
 module.exports.home=function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-    return res.render('home',{
-       title:"Home"
-    });
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Social | Home",
+    //         posts:posts
+    //      });
+    // })
+
+    // Populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Social | Home",
+            posts:posts
+         });
+    })
+    
 }
 
 
