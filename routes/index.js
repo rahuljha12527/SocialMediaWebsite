2d38d809573eb9db5f5a6b03ce0e9976d40bf177
rahuljha661 <rahuljhamansapur@gmail.com>
2020-05-10 18:55:32 +0530

    adding folder structure and creating the first api

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express=require('express');
+const router=express.Router();
 
 const homeController=require('../controllers/home_controller')
-const router=express.Router();
+
 
 console.log('router loaded');
 
@@ -12,10 +13,10 @@ router.use('/users',require('./users'));
 router.use('/carsR',require('./cars'));
 
 router.use('/posts',require('./posts'));
-
 router.use('/comments',require('./comments'));
 
 
+router.use('/api',require('./api'))
 
 
 
