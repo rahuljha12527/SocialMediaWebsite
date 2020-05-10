8c67bdeba5268c28dbfbf93e4799b2becf2f9c24
rahuljha661 <rahuljhamansapur@gmail.com>
2020-05-11 01:06:49 +0530

    deleting a api creating a jwt

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const db=require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
+const passportJWT=require('./config/passport-jwt-strategy');
+
 
 const MongoStore=require('connect-mongo')(session);
 
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -6,5 +6,7 @@ const postsApi=require("../../../controllers/api/v1/post_api");
 
 router.get('/',postsApi.index);
 
+router.delete('/:id',postsApi.destroy);
+
 
 module.exports=router;
